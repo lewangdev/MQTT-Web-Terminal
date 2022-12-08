e510350efba228a37f8e1c0250bb531aeaad191d
Le Wang <lewang.dev@gmail.com>
2022-12-08 14:52:12 +0800

    update

--- main.py
+++ main.py
@@ -7,14 +7,16 @@ import struct
 import fcntl
 import logging
 import json
+import time
+
 import paho.mqtt.client as mqtt
 import threading
 
 
 FORMAT = '%(asctime)s %(message)s'
 logging.basicConfig(format=FORMAT, level=logging.DEBUG)
-pk = "1"
-dk = "11"
+pk = "hWHbMmfnDa"
+dk = "CFE4C09C"
 
 
 class AttrDict(dict):
@@ -23,32 +25,36 @@ class AttrDict(dict):
         self.__dict__ = self
 
 
-app = AttrDict(config=AttrDict(fd=None, cmd="sh", child_pid=None))
+config=AttrDict(fd=None, cmd="sh", child_pid=None)
 
 
 def set_winsize(fd, row, col, xpix=0, ypix=0):
-    logging.debug("Setting window size with termios")
+    logging.debug(f"Setting window size with termios: {row}x{col}")
     winsize = struct.pack("HHHH", row, col, xpix, ypix)
     fcntl.ioctl(fd, termios.TIOCSWINSZ, winsize)
 
 
 def resize(data):
-    if app.config.fd:
+    if config.fd:
         logging.debug(f"Resizing window to {data['rows']}x{data['cols']}")
-        set_winsize(app.config.fd, data["rows"], data["cols"])
+        set_winsize(config.fd, data["rows"], data["cols"])
 
 
 def read_and_forward_pty_output(mqttc):
     max_read_bytes = 1024 * 20
     while True:
-        if app.config.fd:
-            timeout_sec = None
-            (data_ready, _, _) = select.select([app.config.fd], [], [], timeout_sec)
-            logging.debug("Data ready: " + ",".join(map(lambda x: str(x), data_ready)))
+        if config.fd:
+            try:
+                timeout_sec = None
+                (data_ready, _, _) = select.select([config.fd], [], [], timeout_sec)
+                logging.debug("Data ready: " + ",".join(map(lambda x: str(x), data_ready)))
+            except select.error:
+                logging.info("select error")
+                pass
             if data_ready:
-                output = os.read(app.config.fd, max_read_bytes).decode(
+                output = os.read(config.fd, max_read_bytes).decode(
                     errors="ignore"
-                )
+                    )
                 mqttc.publish(f"/user/{pk}/{dk}/terminal/output", json.dumps({"output": output}))
 
 
@@ -56,9 +62,9 @@ def pty_input(data):
     """write to the child pty. The pty sees this as if you are typing in a real
     terminal.
     """
-    if app.config.fd:
+    if config.fd:
         logging.debug("Received input from browser: %s" % data["input"])
-        os.write(app.config.fd, data["input"].encode())
+        os.write(config.fd, data["input"].encode())
 
 
 def mqtt_on_connect(client, userdata, flags, rc):
@@ -91,23 +97,24 @@ if __name__ == "__main__":
         # this is the child process fork.
         # anything printed here will show up in the pty, including the output
         # of this subprocess
-        subprocess.run(app.config.cmd)
+        while True:
+            subprocess.run(config.cmd)
     else:
         # this is the parent process fork.
         # store child fd and pid
-        app.config.fd = fd
-        app.config.child_pid = child_pid
+        config.fd = fd
+        config.child_pid = child_pid
         set_winsize(fd, 50, 50)
 
-    client = mqtt.Client(client_id=f"{pk}.{dk}")
-    client.username_pw_set(username=f"{pk}.{dk}", password=dk)
-    client.on_connect = mqtt_on_connect
-    client.on_message = mqtt_on_message
+        client = mqtt.Client(client_id=f"{pk}.{dk}")
+        client.username_pw_set(username=f"{pk}.{dk}", password=dk)
+        client.on_connect = mqtt_on_connect
+        client.on_message = mqtt_on_message
 
-    client.connect("192.168.52.164", 2883, 60)
+        client.connect("192.168.52.164", 2883, 60)
 
-    # Blocking call that processes network traffic, dispatches callbacks and
-    # handles reconnecting.
-    # Other loop*() functions are available that give a threaded interface and a
-    # manual interface.
-    client.loop_forever()
+        # Blocking call that processes network traffic, dispatches callbacks and
+        # handles reconnecting.
+        # Other loop*() functions are available that give a threaded interface and a
+        # manual interface.
+        client.loop_forever()
